52d8ef5af893d1c7baffccf41a64814aafe47111
Pedro Lucas <90738138+Daeke0n@users.noreply.github.com>
2025-04-03 21:20:35 -0300

    Atividade tesouro

---
+++ Exercicios/Olimpiadas/nivel 1/emoticons.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string a;
+    getline(cin, a);
+
+    int divertido = 0;
+    int chateado = 0;
+
+    for (size_t i = 0; i < a.length() -2; i++) {
+        if (a.substr(i, 3) == ":-)") {
+            divertido++;
+        }else if (a.substr(i, 3) == ":-(") {
+            chateado++;
+        }
+    }
+
+    if (divertido > chateado) {
+        printf("divertido\n");
+    }else if (chateado > divertido) {
+        printf("chateado\n");
+    }else {
+        printf("Neutro\n");
+    }
+}
