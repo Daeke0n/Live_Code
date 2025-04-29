a1fb22a22ef77661d5640a6c3975abd4bff7c7c8
Pedro Lucas <90738138+Daeke0n@users.noreply.github.com>
2025-04-29 14:21:28 -0300

    Update atividade_animal.cpp
    Complete

--- Exercicios/BEECROWD/atividade_animal.cpp
+++ Exercicios/BEECROWD/atividade_animal.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
-#include <cstdio>
+#include <cstdlib>
+#include <cctype>
 
 using namespace std;
 
 int main() {
+    string animais;
+    cin >> animais;
+    string tipodeAnimal;
+    cin >> tipodeAnimal;
+    string alimentacao;
+    cin >> alimentacao;
 
+    if (animais == "vertebrado" && tipodeAnimal == "ave" && alimentacao == "carnivoro") {
+        cout << "aguia" << endl;
+    } else if (animais == "vertebrado" && tipodeAnimal == "ave" && alimentacao == "onivoro") {
+        cout << "pomba" << endl;
+    }
+    if (animais == "vertebrado" && tipodeAnimal == "mamifero" && alimentacao == "onivoro") {
+        cout << "homem" << endl;
+    } else if (animais == "vertebrado" && tipodeAnimal == "mamifero" && alimentacao == "herbivoro") {
+        cout << "vaca" << endl;
+    }
+    if (animais == "invertebrado" && tipodeAnimal == "inseto" && alimentacao == "hematofago") {
+        cout << "pulga" << endl;
+    }else if (animais == "invertebrado" && tipodeAnimal == "inseto" && alimentacao == "herbivoro") {
+        cout << "lagarta" << endl;
+    }
+    if (animais == "invertebrado" && tipodeAnimal == "anelideo" && alimentacao == "hematofago") {
+        cout << "sanguessuga" << endl;
+    }else if (animais == "invertebrado" && tipodeAnimal == "anelideo" && alimentacao == "onivoro") {
+        cout << "minhoca" << endl;
+    }
 }
